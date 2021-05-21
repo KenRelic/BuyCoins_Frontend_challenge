a4d3f893245d8ab52886d35d8ff83c1cff6a8c3a
kenRelic <simeonkenneth3@gmail.com>
2021-05-21 03:05:52 +0100

    feat: adds main section and initial styling

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const menuBarBtn = document.getElementById("mobile-menu-btn");
 const mobileNavBar = document.getElementById("mobile-navbar");
 function toggleNavBar() {
-  //   console.log(this);
   mobileNavBar.classList.toggle("open");
 }
 
